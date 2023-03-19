1feeca453a295fa758d6c7b1a36b80ec2998ac63
Aleksander <asava@sibmail.com>
2023-03-20 00:22:43 +0700

    Указание дочерних компонентов

---
+++ src/component/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Counter = () => {
+	return (
+		<React.Fragment>
+			<h1>Counter</h1>
+			<button>+</button>
+		</React.Fragment>
+	)
+}
+
+export default Counter
