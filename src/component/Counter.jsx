24b80d1de23622e8d112e058930dfd307447e41a
Aleksander <asava@sibmail.com>
2023-03-20 00:43:55 +0700

    Вложенные выражения

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Counter = () => {
+	const count = 2
+	const x = <h1>empty</h1>
+	const formatCount = () => {
+		return count === 0 ? x : count
+	}
 	return (
 		<React.Fragment>
-			<h1>Counter</h1>
+			<h1>{formatCount()}</h1>
 			<button>+</button>
 		</React.Fragment>
 	)
