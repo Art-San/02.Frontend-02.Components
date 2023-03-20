bcd4887168365a6f966ef89b58052dda4524a077
Aleksander <asava@sibmail.com>
2023-03-20 16:21:19 +0700

    Обработка событий

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
 
 const Counter = () => {
-	const count = 0
+	let count = 0
 
 	const formatCount = () => {
 		return count === 0 ? 'empty' : count
 	}
 
-	const classes = count === 0 ? 'bg-warning' : 'bg-primary'
-	const imageUrl = 'https://picsum.photos/200'
-
 	const getBageClasses = () => {
-		let classes1 = 'badge m-2 '
-		classes1 += count === 0 ? 'bg-warning' : 'bg-primary'
-		// classes1 = classes1 + (count === 0 ? 'bg-warning' : 'bg-primary')
-		return classes1
+		let classes = 'badge m-2 '
+		classes += count === 0 ? 'bg-warning' : 'bg-primary'
+		return classes
+	}
+
+	const handleIncrement = () => {
+		count = count + 1
+		console.log('count', count)
 	}
 
 	return (
-		<React.Fragment>
-			<img src={imageUrl} alt='home' />
-			<span className={`badge ${classes} m-2`}>{formatCount()}</span>
+		<>
 			<span className={getBageClasses()}>{formatCount()}</span>
-			<button className='btn btn-primary btn-sm'>+</button>
-		</React.Fragment>
+			<button
+				className='btn btn-primary btn-sm'
+				onClick={handleIncrement}
+			>
+				+
+			</button>
+		</>
 	)
 }
 
