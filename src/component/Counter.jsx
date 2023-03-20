5f58f45f37d319a6055ba81ee2d2d659785b5a39
Aleksander <asava@sibmail.com>
2023-03-20 19:04:56 +0700

    Рендер списка из массива

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 const Counter = () => {
 	const [count, setCount] = useState(0)
+	const [tags, setTegs] = useState(['tag1', 'teg2', 'teg3'])
 
 	const formatCount = () => {
 		return count === 0 ? 'empty' : count
@@ -23,8 +24,25 @@ const Counter = () => {
 		setCount(prevState => prevState - 1)
 	}
 
+	const handleTagChange = () => {
+		setTegs(['tag4', 'teg5'])
+	}
+
 	return (
 		<>
+			<ul>
+				{tags?.map((tag, i) => {
+					return (
+						<li
+							key={i}
+							className='btn btn-primary btn-sm m-2'
+							onClick={handleTagChange}
+						>
+							{tag}
+						</li>
+					)
+				})}
+			</ul>
 			<span className={getBageClasses()}>{formatCount()}</span>
 			<button
 				className='btn btn-primary btn-sm m-2'
